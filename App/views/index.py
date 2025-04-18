f63f250a2d7853037036aa728ea506b2fb3f6e0e
Jamal Mohammed <jamalm2427@gmail.com>
2025-04-17 21:56:18 -0400

    deletion

--- App/views/index.py
+++ App/views/index.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, url_for
-from App.controllers import create_user, initialize, get_recipes
+from App.controllers import create_user, initialize
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
