c0dd3953eca63268601d37c4b700f1c46c785181
Jamal Mohammed <jamalm2427@gmail.com>
2025-04-17 21:17:28 -0400

    added automatic init capability

--- App/views/index.py
+++ App/views/index.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
+from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, url_for
 from App.controllers import create_user, initialize
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 @index_views.route('/', methods=['GET'])
+def init_route():
+    initialize()
+    return redirect(url_for('index_views.login_page'))
+
+@index_views.route('/login', methods=['GET'])
 def login_page():
     return render_template('login.html')
 
