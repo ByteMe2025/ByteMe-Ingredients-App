43b2242d3a6aec0d46e8b958f4d6badf33a9078f
Jamal Mohammed <jamalm2427@gmail.com>
2025-04-19 23:14:15 -0400

    error removal

--- App/views/index.py
+++ App/views/index.py
@@ -1,7 +1,6 @@
 from sqlite3 import IntegrityError
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify, url_for
 from App.controllers import create_user, initialize
-from App.models.user import Ingredient, UserIngredients, User, Recipe, UserRecipes
 import requests
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
